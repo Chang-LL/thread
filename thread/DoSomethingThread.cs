24ade3caf3f41c630783c706a86f59bc4c7a9f22
cl <13546417565@163.com>
2018-01-31 00:35:48 +0800

    2

---
+++ thread/DoSomethingThread.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyThread
+{
+    class DoSomethingThread
+    {
+        static void WorkerMethod()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("Worker Thread:" + i.ToString());
+            }
+        }
+        static void Main()
+        {
+            ThreadStart ts = new ThreadStart(WorkerMethod);
+            Thread t = new Thread(ts);
+            t.Start();
+            for (int i = 1; i < 1000; i++)
+            {
+                Console.WriteLine("Primary Thread:" + i.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- thread/SimpleThread.cs
+++ thread/SimpleThread.cs
@@ -2,14 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MyThread
 {
-    class SimpleThread
+    public class SimpleThread
     {
-        int i = 5;
-        int x = 10;
+        public void SimpleMethod()
+        {
+            int i = 5;
+            int x = 10;
+            int result = i*x;
+            Console.WriteLine("This code calculated the value"
+                + result.ToString() + "from thread ID:"
+                + AppDomain.GetCurrentThreadId().ToString());
+        }
+        public static void Main0()
+        {
+            //Calling the method from our current thread
+            SimpleThread simpleThread = new SimpleThread();
+            simpleThread.SimpleMethod();
+
+            //Calling the method on a new thread
+            ThreadStart ts = new ThreadStart(simpleThread.SimpleMethod);
+            Thread t = new Thread(ts);
+            t.Start();
+            Console.ReadLine();
+        }
 
     }
 }
