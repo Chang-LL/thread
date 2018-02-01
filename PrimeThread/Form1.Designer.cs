52d4c8137818f03872309d246b7a346bae861443
cl <13546417565@163.com>
2018-02-01 10:57:59 +0800

    线程操作

---
+++ PrimeThread/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace PrimeThread
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonPause = new System.Windows.Forms.Button();
+            this.buttonResume = new System.Windows.Forms.Button();
+            this.listViewPrime = new System.Windows.Forms.ListView();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(44, 439);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(76, 34);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // buttonPause
+            // 
+            this.buttonPause.Location = new System.Drawing.Point(184, 439);
+            this.buttonPause.Name = "buttonPause";
+            this.buttonPause.Size = new System.Drawing.Size(71, 34);
+            this.buttonPause.TabIndex = 1;
+            this.buttonPause.Text = "Pause";
+            this.buttonPause.UseVisualStyleBackColor = true;
+            this.buttonPause.Click += new System.EventHandler(this.ButtonPause_Click);
+            // 
+            // buttonResume
+            // 
+            this.buttonResume.Location = new System.Drawing.Point(317, 439);
+            this.buttonResume.Name = "buttonResume";
+            this.buttonResume.Size = new System.Drawing.Size(84, 34);
+            this.buttonResume.TabIndex = 2;
+            this.buttonResume.Text = "Resume";
+            this.buttonResume.UseVisualStyleBackColor = true;
+            this.buttonResume.Click += new System.EventHandler(this.buttonResume_Click);
+            // 
+            // listViewPrime
+            // 
+            this.listViewPrime.Location = new System.Drawing.Point(12, 21);
+            this.listViewPrime.Name = "listViewPrime";
+            this.listViewPrime.Size = new System.Drawing.Size(451, 397);
+            this.listViewPrime.TabIndex = 3;
+            this.listViewPrime.UseCompatibleStateImageBehavior = false;
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(419, 445);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 4;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 474);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.listViewPrime);
+            this.Controls.Add(this.buttonResume);
+            this.Controls.Add(this.buttonPause);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Prime Numbers";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonPause;
+        private System.Windows.Forms.Button buttonResume;
+        private System.Windows.Forms.ListView listViewPrime;
+        private System.Windows.Forms.Button buttonStop;
+    }
+}
+
---
+++ PrimeThread/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimeThread
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //private thread variable
+        private Thread primeNumberThread;
+        public delegate void UpdateData(string returnVal);
+        public void UpdateUI(string val)
+        {
+            listViewPrime.Items.Add(val);
+        }
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            //Let's create a new thread
+            primeNumberThread = new Thread(
+                new ThreadStart(GeneratePrimeNumbers));
+            //let's give a name for the thread
+            primeNumberThread.Name = "Prime Numbers Example";
+            primeNumberThread.Priority = ThreadPriority.BelowNormal;
+            //Enable the Pause Button
+            buttonPause.Enabled = true;
+            //disable the start button
+            buttonStart.Enabled = false;
+            primeNumberThread.Start();
+        }
+
+        private void GeneratePrimeNumbers()
+        {
+            long lngCounter;
+            long lngNumber;
+            long lngDivideByCounter;
+            bool blnIsPrime;
+            long[] PrimeArray = new long[255];
+            string[] args = new string[] { "2" };
+            UpdateData UIdel = new UpdateData(UpdateUI);
+            //initialize variables
+            lngNumber = 3;
+            lngCounter = 2;
+            PrimeArray[1] = 2;
+            Invoke(UIdel, args);
+            //listViewPrime.Items.Add(2.ToString());
+            while (lngCounter < 256)
+            {
+                blnIsPrime = true;
+                for (lngDivideByCounter = 1; PrimeArray[lngDivideByCounter]
+                    * PrimeArray[lngDivideByCounter] <= lngNumber;
+                    lngDivideByCounter++)
+                {
+                    if (lngNumber % PrimeArray[lngDivideByCounter] == 0)
+                    {
+                        //this is not a prime number
+                        blnIsPrime = false;
+                        break;
+                    }
+                }
+                //if this is a prime number then display it
+                if (blnIsPrime)
+                {
+                    PrimeArray[lngCounter] = lngNumber;
+                    //increase prime found count;
+                    lngCounter++;
+                    args[0] = lngNumber.ToString();
+                    Invoke(UIdel, args);
+                    //listViewPrime.Items.Add(lngNumber.ToString());
+                    //put the thread to sleep 100 milliseconds
+                    //this will simulate the time lag and we'll get time
+                    //to pause and resume the thread
+                    Thread.Sleep(100);
+                }
+                //increment number by 2;
+                lngNumber += 2;
+            }
+            //once the thread is finished execution enable the start
+            //and disable the pause button
+            buttonStart.Enabled = true;
+            buttonPause.Enabled = true;
+        }
+
+        private void ButtonPause_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                //if current state of thread is running
+                //then pause the thread
+                //???
+                if (!(primeNumberThread.ThreadState== ThreadState.Running))
+                {
+
+                    //pause the thread
+                    primeNumberThread.Suspend();
+                    //disable the pause button
+                    buttonPause.Enabled = false;
+                    //enable the resume button
+                    buttonResume.Enabled = true;
+            }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Exception",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error,
+                    MessageBoxDefaultButton.Button1);
+            }
+
+        }
+
+        private void buttonResume_Click(object sender, EventArgs e)
+        {
+            if (primeNumberThread.ThreadState == ThreadState.Suspended
+                || primeNumberThread.ThreadState == ThreadState.SuspendRequested)
+            {
+                try
+                {
+                    //resume the thread
+                    primeNumberThread.Resume();
+                    buttonResume.Enabled = false;
+                    buttonPause.Enabled = true;
+                }
+                catch (ThreadStateException ex)
+                {
+                    MessageBox.Show(ex.ToString(), "Exception",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error,
+                        MessageBoxDefaultButton.Button1);
+                }
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            //enable the start button and disable all others
+            buttonStart.Enabled = true;
+            buttonStop.Enabled = false;
+            buttonPause.Enabled = false;
+            buttonResume.Enabled = false;
+            //destroy the thread
+            if (primeNumberThread.ThreadState == ThreadState.Suspended
+                || primeNumberThread.ThreadState == ThreadState.SuspendRequested)
+                primeNumberThread.Resume();
+            primeNumberThread.Abort();
+        }
+    }
+}
